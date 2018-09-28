34c913d8f153fb65ad655ad559316bb746d9ea98
yinyefeng <clickr@qq.com>
2018-09-28 17:55:39 +0800

    9281755

--- nupkg/push.ps1
+++ nupkg/push.ps1
@@ -3,6 +3,6 @@ $nupkgFiles = (Get-Item *.nupkg).FullName
 
 # Copy all nuget packages to the pack folder
 foreach($nupkgFile in $nupkgFiles) {
-    nuget push $nupkgFile -Source http://121.40.41.127:8003/nuget -ApiKey 12345trewq
+    nuget push $nupkgFile -Source http://121.40.41.127:8003/nuget -ApiKey nu861@928
     Remove-Item $nupkgFile
 }
